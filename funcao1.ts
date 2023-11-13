df5153df160bdd7bbaf5f37cdd00aa76400d7cf9
Yuji <g.yujinak@gmail.com>
2023-11-13 18:34:42 -0300

    first commit

---
+++ funcao1.ts
@@ -0,0 +1,6 @@
+const multiplicar = function (n1: number, n2: number): number {
+  return n1 * n2;
+};
+
+console.log(multiplicar(4, 3));
+console.log(multiplicar(10, 18));
---
+++ funcao2.ts
@@ -0,0 +1,6 @@
+const saudacao = function (nome: string): string {
+  return `Olá, ${nome}`;
+};
+
+console.log(saudacao("José"));
+console.log(saudacao("Maria"));
